1c67a8e1a813cfad3cff76883de4147fb7e49967
Daniel Veiga <daveigantu@gmail.com>
2017-08-04 21:05:45 -0300

    Add project files.

---
+++ WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(113, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(240, 47);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(113, 196);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(240, 47);
+            this.axWindowsMediaPlayer2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Começar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(498, 290);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 287);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.axWindowsMediaPlayer2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition;
+using System.Threading;
+using System.Media;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer explosao = new SoundPlayer(@"c:\imagem\explosao.wav");
+        SoundPlayer desvio = new SoundPlayer(@"c:\imagem\desvio.wav");
+        SoundPlayer esquerda = new SoundPlayer(@"c:\imagem\esquerda.wav");
+        SoundPlayer direita = new SoundPlayer(@"c:\imagem\direita.wav");
+        SoundPlayer impesq = new SoundPlayer(@"c:\imagem\impdir.wav");
+        SoundPlayer impdir = new SoundPlayer(@"c:\imagem\imesq.wav");
+        int vida;
+        int tempo;
+        int n;
+        int t;
+        int l;
+        private static SpeechRecognitionEngine engine;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void LoadSpeech()
+        {
+            try
+            {
+                engine = new SpeechRecognitionEngine();
+                engine.SetInputToDefaultAudioDevice();
+
+                string[] words = { "esquerda", "direita" };
+
+                engine.LoadGrammar(new Grammar(new GrammarBuilder(new Choices(words))));
+                engine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(rec);
+                engine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("ocorreu um erro: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = @"c:\imagem\fundo.wav";
+            axWindowsMediaPlayer1.settings.playCount = 1000;
+            LoadSpeech();
+        }
+
+        private void rec(object s, SpeechRecognizedEventArgs e)
+        {
+            if (vida < 5)
+            {
+                tempo = 0;//seta a variável de tempo
+                timer1.Stop();
+                switch (e.Result.Text)
+                {
+                    case "esquerda":
+                        if (l == 0)
+                        {
+                            impesq.Play();
+                            vida++;
+                            sirene();
+                            verifica();
+                        }
+                        else
+                        {
+                            desvio.Play();
+                            ataque();
+                        }
+                        break;
+                    case "direita":
+                        if (l == 1)
+                        {
+                            impdir.Play();
+                            vida++;
+                            sirene();
+                            verifica();
+                        }
+                        else
+                        {
+                            desvio.Play();
+                            ataque();
+                        }
+                        break;
+                       
+                }
+            }
+            
+        }
+        private void ataque()
+        {
+               
+                Random randNum = new Random();
+                n = randNum.Next(2); //gera numero aleatório
+                tempo = 0;//seta a variável de tempo
+                //n = 0; //linha para teste (tira a aleatoriedade, sempre será esquerda)
+
+                Thread.Sleep(1500);
+                if (n == 0)
+                {
+                    esquerda.Play();
+                    timer1.Start();
+                    l = n;
+                }
+
+                if (n == 1)
+                {
+                    direita.Play();
+                    timer1.Start();
+                    l = n;
+                }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ataque();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tempo++;//adiciona 1 a cada 1 segundo a variavel tempo
+            if (tempo == 4)//tempo de reação (do disparo até o impacto)
+            {
+                if (l == 0)
+                {
+                    impesq.Play();
+                    vida++;
+                }
+                else
+                {
+                    impdir.Play();
+                    vida++;
+                }
+                sirene();
+                verifica();
+            }
+        }
+        private void sirene()
+        {
+            if (vida == 3 && t != 1)//ativa alerta restam 2 vidas
+            {
+                t = 1;
+                axWindowsMediaPlayer2.URL = @"c:\imagem\sirene.mp4";
+                axWindowsMediaPlayer2.settings.playCount = 2000;
+            }
+        }
+        private void verifica()
+        {
+            if (vida < 5)//quantidade de vidas
+            {
+                ataque();
+            }
+            else //game over
+            {
+                vida = 6;
+                axWindowsMediaPlayer2.Ctlcontrols.stop();
+                axWindowsMediaPlayer1.Ctlcontrols.stop();
+                explosao.Play();
+                axWindowsMediaPlayer2.settings.playCount = 0;
+                axWindowsMediaPlayer2.URL = @"c:\imagem\fimdejogo.mp3";
+                axWindowsMediaPlayer2.settings.volume = 2000;
+            }
+        }
+    }
+}
