fea5e38ab94b0a30d5ea68a936c142ddde63066c
Daniel Veiga <daveigantu@gmail.com>
2017-08-07 21:41:56 -0300

    1.0(comentarios adicionados)

--- WindowsFormsApplication5/Form1.cs
+++ WindowsFormsApplication5/Form1.cs
@@ -6,25 +6,25 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using Microsoft.Speech.Recognition;
+using Microsoft.Speech.Recognition; //biblioteca usada para o reconhecimento
 using System.Threading;
-using System.Media;
+using System.Media; //biblioteca usada para reprodução de audios
 
 namespace WindowsFormsApplication5
 {
     public partial class Form1 : Form
     {
-        SoundPlayer explosao = new SoundPlayer(@"c:\imagem\explosao.wav");
-        SoundPlayer desvio = new SoundPlayer(@"c:\imagem\desvio.wav");
-        SoundPlayer esquerda = new SoundPlayer(@"c:\imagem\esquerda.wav");
-        SoundPlayer direita = new SoundPlayer(@"c:\imagem\direita.wav");
-        SoundPlayer impesq = new SoundPlayer(@"c:\imagem\impdir.wav");
-        SoundPlayer impdir = new SoundPlayer(@"c:\imagem\imesq.wav");
-        int vida;
-        int tempo;
-        int n;
-        int t;
-        int l;
+        SoundPlayer explosao = new SoundPlayer(@"c:\imagem\explosao.wav"); //explosão final de game over
+        SoundPlayer desvio = new SoundPlayer(@"c:\imagem\desvio.wav"); //desvio de nave
+        SoundPlayer esquerda = new SoundPlayer(@"c:\imagem\esquerda.wav"); //tiro vindo da esquerda
+        SoundPlayer direita = new SoundPlayer(@"c:\imagem\direita.wav"); //tiro vindo da direita
+        SoundPlayer impesq = new SoundPlayer(@"c:\imagem\impdir.wav"); //impacto na esquerda
+        SoundPlayer impdir = new SoundPlayer(@"c:\imagem\imesq.wav");// impacto na direita
+        int vida; //do jogador
+        int tempo; //intervalo de reação
+        int n;//variável que armazena numero aleatório para ataque
+        int t;//variável de segurança, usada na verificação da sirene
+        int l;// variável de segurança de dado, sempre recebe valor de 'n'
         private static SpeechRecognitionEngine engine;
         public Form1()
         {
@@ -32,21 +32,21 @@ namespace WindowsFormsApplication5
         }
         private void LoadSpeech()
         {
-                engine = new SpeechRecognitionEngine();
-                engine.SetInputToDefaultAudioDevice();
+                engine = new SpeechRecognitionEngine(); //caso haja erro nesta linha o problema é com os pacotes, verificar também a versão do sistema: 32 ou 64 bits
+                engine.SetInputToDefaultAudioDevice(); //caso haja erro nesta linha significa que o programa não conseguiu localizar um microfone para ser utilizado no reconhecimento
 
-                string[] words = { "esquerda", "direita" };
+                string[] words = { "esquerda", "direita" };//gramática, novas palavras a serem reconhecidas devem ser introduzidas aqui
 
-                engine.LoadGrammar(new Grammar(new GrammarBuilder(new Choices(words))));
+                engine.LoadGrammar(new Grammar(new GrammarBuilder(new Choices(words))));//carrega gramática
                 engine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(rec);
                 engine.RecognizeAsync(RecognizeMode.Multiple);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            axWindowsMediaPlayer1.URL = @"c:\imagem\fundo.wav";
-            axWindowsMediaPlayer1.settings.playCount = 1000;
-            LoadSpeech();
+            axWindowsMediaPlayer1.URL = @"c:\imagem\fundo.wav"; //som de fundo
+            axWindowsMediaPlayer1.settings.playCount = 1000; //quantidade de repetição do audio
+            LoadSpeech(); //inicia o reconhecimento
         }
 
         private void rec(object s, SpeechRecognizedEventArgs e)
@@ -55,7 +55,7 @@ namespace WindowsFormsApplication5
             {
                 tempo = 0;//seta a variável de tempo
                 timer1.Stop();//para o timer
-                switch (e.Result.Text)
+                switch (e.Result.Text) //variável que possui o resultado obtido no reconhecimento
                 {
                     case "esquerda":
                         if (l == 0)
@@ -90,7 +90,7 @@ namespace WindowsFormsApplication5
             }
             
         }
-        private void ataque()
+        private void ataque()//ataque da nave inimiga
         {
                
                 Random randNum = new Random();
@@ -98,18 +98,18 @@ namespace WindowsFormsApplication5
                 tempo = 0;//seta a variável de tempo
                 //n = 0; //linha para teste (tira a aleatoriedade, sempre será esquerda)
 
-                Thread.Sleep(1500);
+                Thread.Sleep(1500);//intervalo para carregar audio
                 if (n == 0)
                 {
                     esquerda.Play();
-                    timer1.Start();
+                    timer1.Start(); //inicia o timer
                     l = n;
                 }
 
                 if (n == 1)
                 {
                     direita.Play();
-                    timer1.Start();
+                    timer1.Start(); //inicia o timer
                     l = n;
                 }
         }
@@ -138,18 +138,18 @@ namespace WindowsFormsApplication5
                 verifica();
             }
         }
-        private void sirene()
+        private void sirene()//ativa sons de sirene depois de três impactos
         {
-            if (vida == 3 && t != 1)//ativa alerta restam 2 vidas
+            if (vida == 3 && t != 1)
             {
-                t = 1;
+                t = 1;//impede reprodução repitida
                 axWindowsMediaPlayer2.URL = @"c:\imagem\sirene.mp4";
                 axWindowsMediaPlayer2.settings.playCount = 2000;
             }
         }
-        private void verifica()
+        private void verifica()//verifica se jogador perdeu, caso contrário chama ataque() novamente
         {
-            if (vida < 5)//quantidade de vidas
+            if (vida < 5)//para alterar quantidade de vidas do jogador modifique esta linha
             {
                 ataque();
             }
